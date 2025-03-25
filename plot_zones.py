936b5b00d6e798978e31c3d89aaaed4dcd5051d1
Julian Quick <juqu@dtu.dk>
2025-03-25 11:03:51 +0100

    initial commit

---
+++ plot_zones.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import h5py
+from precompute_farm_layouts import load_farm_boundaries
+
+plt.figure(figsize=(10, 10))
+plt.gca().set_aspect('equal', adjustable='box')
+plt.draw()
+zones = load_farm_boundaries()
+for zz, zone_coords in enumerate(zones):
+    plt.plot(zone_coords[0], zone_coords[1])
+    plt.fill(zone_coords[0], zone_coords[1], alpha=0.2, label=zz)
+
+SIZE = 2
+
+with h5py.File('precomputed_layouts.h5', 'r') as f:
+   for key in f.keys():
+       if 'farm' not in key: continue
+       farm = int(key.split('_')[0].strip('farm'))
+       seed = int(key.split('_')[2].strip('s'))
+       if farm == 0 and seed > 0: continue
+       x, y = f[key]['layout']
+       if farm == 0 and seed == 0:
+           plt.scatter(x, y, c='k', alpha=1, s=SIZE)
+       else:
+           plt.scatter(x, y, c='k', alpha=0.003, s=SIZE)
+plt.legend()
+plt.savefig('zones_updated')
+plt.clf()
+
+
